45e15d39cb5224361abda47a26bc3bcc68ba68eb
Jon Patterson <JonPatt92@gmail.com>
2020-01-22 13:35:50 -0700

    Complete Ogre all tests passing

--- exercises/human.js
+++ exercises/human.js
@@ -1,8 +1,29 @@
 class Human {
   constructor(name) {
-      this.name = name;
+      this.name             = name;
+      this.encounterCounter = 0;
+      this.knockedOut       = false;
     }
 
+  knockout(){
+    this.knockedOut = true;
+  }
+
+  recover(){
+    this.knockedOut = false;
+  }
+
+  encounter() {
+    this.encounterCounter += 1;
+  }
+
+  noticesOgre() {
+    if (this.encounterCounter % 3 === 0 && this.encounterCounter > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Human;
--- exercises/ogre.js
+++ exercises/ogre.js
@@ -1,8 +1,27 @@
 class Ogre {
   constructor(name, home = 'Swamp') {
-      this.name = name;
-      this.home = home;
+      this.name   = name;
+      this.home   = home;
+      this.swings = 0;
     }
+
+  swingAt(human) {
+    this.swings += 1;
+    if (this.swings % 2 === 0 && this.swings > 0) {
+      human.knockout();
+    }
+  }
+
+  encounter(human) {
+    human.encounter();
+    if (human.noticesOgre() === true) {
+      this.swingAt(human);
+    }
+  }
+
+  apologize(human) {
+    human.recover();
+  }
 
 }
 
